c71c354cfcd894df0562fb66d59c13b4a9e9376c
Anton Tkachuk <tkacuk2291@gmail.com>
2023-10-26 10:50:06 +0300

    Solution

--- main.py
+++ main.py
@@ -6,15 +6,20 @@ from db.models import Actor
 
 
 def main() -> QuerySet:
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    genre_list = ["Western", "Action", "Dramma"]
+    [Genre.objects.create(name=genre_name) for genre_name in genre_list]
+
+    actor_list = [
+        {"first_name": "George", "last_name": "Klooney"},
+        {"first_name": "Kianu", "last_name": "Reaves"},
+        {"first_name": "Scarlett", "last_name": "Keegan"},
+        {"first_name": "Will", "last_name": "Smith"},
+        {"first_name": "Jaden", "last_name": "Smith"},
+        {"first_name": "Scarlett", "last_name": "Johansson"},
+
+    ]
+    [Actor.objects.create(**actor) for actor in actor_list]
+
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
     (Actor.objects.filter(first_name="Kianu", last_name="Reaves")
